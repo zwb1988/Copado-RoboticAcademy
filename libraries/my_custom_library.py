bfba4586071a241e983da78a17f33dd8404e5851
Wenbo Zhou <wenbo.w.zhou@pwc.com>
2022-04-17 17:03:16 +0800

    Added a python lib

---
+++ libraries/my_custom_library.py
@@ -0,0 +1,7 @@
+from robot.api import logger
+
+class my_custom_library():
+    def print_message(self, message):
+        """Prints a custom message to log
+        """
+        logger.info(message)
